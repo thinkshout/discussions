cb3752084725df4785a9d72a04fb9bc6e54d23f5
ruscoe <danruscoe@gmail.com>
2016-08-18 10:55:42 -0700

    Add fields to Discussion Group Member.

--- src/Entity/DiscussionGroupMember.php
+++ src/Entity/DiscussionGroupMember.php
@@ -3,6 +3,8 @@
 namespace Drupal\discussions\Entity;
 
 use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
 use Drupal\discussions\DiscussionGroupMemberInterface;
 
 /**
@@ -35,5 +37,54 @@ use Drupal\discussions\DiscussionGroupMemberInterface;
  * )
  */
 class DiscussionGroupMember extends ContentEntityBase implements DiscussionGroupMemberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+    $fields = parent::baseFieldDefinitions($entity_type);
+
+    $fields['discussion_group'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Discussion Group'))
+      ->setRequired(TRUE)
+      ->setSetting('target_type', 'discussion_group');
+
+    $fields['user'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('User'))
+      ->setRequired(TRUE)
+      ->setSetting('target_type', 'user')
+      ->setSetting('handler', 'default')
+      ->setDisplayOptions('form', [
+        'type' => 'options_select',
+        'weight' => 0,
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['pending'] = BaseFieldDefinition::create('boolean')
+      ->setLabel(t('Pending Approval'))
+      ->setDescription(t('A boolean indicating whether the Discussion Group Member is pending approval.'))
+      ->setDefaultValue(FALSE)
+      ->setDisplayOptions('form', array(
+        'type' => 'boolean_checkbox',
+        'settings' => array(
+          'display_label' => TRUE,
+        ),
+        'weight' => 0,
+      ))
+      ->setDisplayConfigurable('form', TRUE);
+
+    $fields['created'] = BaseFieldDefinition::create('created')
+      ->setLabel(t('Created'))
+      ->setDescription(t('The time that the Discussion Group Member was created.'))
+      ->setRevisionable(TRUE);
+
+    $fields['changed'] = BaseFieldDefinition::create('changed')
+      ->setLabel(t('Changed'))
+      ->setDescription(t('The time that the Discussion Group Member was last edited.'))
+      ->setRevisionable(TRUE);
+
+    return $fields;
+  }
 
 }
