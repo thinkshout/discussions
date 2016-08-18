fdad43095b8b47322e884d5726768769931a4ba5
ruscoe <danruscoe@gmail.com>
2016-08-18 10:56:11 -0700

    Set up Discussion Group Member form to auto-populate Discussion Group, redirect to new entity.

--- src/Form/DiscussionGroupMemberForm.php
+++ src/Form/DiscussionGroupMemberForm.php
@@ -17,22 +17,29 @@ class DiscussionGroupMemberForm extends ContentEntityForm {
    */
   public function save(array $form, FormStateInterface $form_state) {
     $entity = $this->entity;
+
+    $discussion_group = \Drupal::routeMatch()->getParameter('discussion_group');
+    $entity->set('discussion_group', $discussion_group);
+
     $status = parent::save($form, $form_state);
 
     switch ($status) {
       case SAVED_NEW:
-        drupal_set_message($this->t('Created %label.', [
+        drupal_set_message($this->t('Created %label.', array(
           '%label' => $entity->label(),
-        ]));
+        )));
         break;
 
       default:
-        drupal_set_message($this->t('Saved %label.', [
+        drupal_set_message($this->t('Saved %label.', array(
           '%label' => $entity->label(),
-        ]));
+        )));
     }
 
-    $form_state->setRedirect('entity.discussion_group_member.canonical', ['discussion_group_member' => $entity->id()]);
+    $form_state->setRedirect('entity.discussion_group_member.canonical', array(
+      'discussion_group' => $discussion_group->id(),
+      'discussion_group_member' => $entity->id(),
+    ));
   }
 
 }
