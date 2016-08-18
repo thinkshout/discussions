e8e550b1d8ca06f050556f144f600623cf0160cb
ruscoe <danruscoe@gmail.com>
2016-08-18 08:55:36 -0700

    Add routes for Discussion Group Members.

--- src/Entity/DiscussionGroupMember.php
+++ src/Entity/DiscussionGroupMember.php
@@ -13,11 +13,25 @@ use Drupal\discussions\DiscussionGroupMemberInterface;
  * @ContentEntityType(
  *   id = "discussion_group_member",
  *   label = @Translation("Discussion Group Member"),
+ *   handlers = {
+ *     "form" = {
+ *       "add" = "Drupal\discussions\Form\DiscussionGroupMemberForm",
+ *       "edit" = "Drupal\discussions\Form\DiscussionGroupMemberForm",
+ *       "delete" = "Drupal\discussions\Form\DiscussionGroupMemberDeleteForm",
+ *     }
+ *   },
  *   base_table = "discussion_group_members",
+ *   admin_permission = "administer discussion group members",
  *   entity_keys = {
  *     "id" = "id",
  *     "uuid" = "uuid"
- *   }
+ *   },
+ *   links = {
+ *     "canonical" = "/admin/config/discussions/group/{discussion_group}/member/{discussion_group_member}",
+ *     "add-form" = "/admin/config/discussions/group/{discussion_group}/member/add",
+ *     "edit-form" = "/admin/config/discussions/group/{discussion_group}/member/{discussion_group_member}/edit",
+ *     "delete-form" = "/admin/config/discussions/group/{discussion_group}/member/{discussion_group_member}/delete",
+ *   },
  * )
  */
 class DiscussionGroupMember extends ContentEntityBase implements DiscussionGroupMemberInterface {
