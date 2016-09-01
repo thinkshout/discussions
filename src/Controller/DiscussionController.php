80518071d420e4958dac1b87e8a51e305814a1f0
ruscoe <danruscoe@gmail.com>
2016-09-01 15:11:19 -0700

    Remove theme for discussion type list - now handled by groups.

--- src/Controller/DiscussionController.php
+++ src/Controller/DiscussionController.php
@@ -43,36 +43,6 @@ class DiscussionController extends ControllerBase {
   }
 
   /**
-   * Displays add links for available bundles/types for Discussions.
-   *
-   * @param \Symfony\Component\HttpFoundation\Request $request
-   *   The current request object.
-   *
-   * @return array
-   *   A render array for a list of the Discussion bundles/types that can be
-   *   added or if there is only one bundle/type defined for the site, the
-   *   function returns the add page for that bundle/type.
-   */
-  public function add(Request $request) {
-    $types = $this->typeStorage->loadMultiple();
-
-    if ($types && count($types) == 1) {
-      $type = reset($types);
-      return $this->addForm($type, $request);
-    }
-
-    if (count($types) === 0) {
-      return array(
-        '#markup' => $this->t('You have not created any Discussion types yet. @link to add a new type.', array(
-          '@link' => $this->l($this->t('Go to the Discussion Type creation page'), Url::fromRoute('discussion_type.add_form')),
-        )),
-      );
-    }
-
-    return array('#theme' => 'discussions_discussion_add_list', '#content' => $types);
-  }
-
-  /**
    * Presents the creation form for Discussions of given bundle/type.
    *
    * @param EntityInterface $discussion_type
