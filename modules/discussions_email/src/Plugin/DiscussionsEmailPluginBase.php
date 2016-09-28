7e92b9e5967b538c44b226cd4c9f161eab20f8c0
ruscoe <danruscoe@gmail.com>
2016-09-27 20:35:18 -0700

    Add base configuration for discussions email plugins.

--- modules/discussions_email/src/Plugin/DiscussionsEmailPluginBase.php
+++ modules/discussions_email/src/Plugin/DiscussionsEmailPluginBase.php
@@ -13,4 +13,48 @@ use Drupal\Core\Plugin\PluginBase;
  */
 abstract class DiscussionsEmailPluginBase extends PluginBase implements DiscussionsEmailPluginInterface {
 
+  /**
+   * {@inheritdoc}
+   */
+  public function getConfiguration() {
+    return $this->configuration;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setConfiguration(array $configuration) {
+    // Merge in the default configuration.
+    $this->configuration = NestedArray::mergeDeep(
+      $this->defaultConfiguration(),
+      $configuration
+    );
+
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    // TODO: Any email plugin configuration.
+    return [];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    // TODO: Any email plugin configuration fields.
+    $form = [];
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function calculateDependencies() {
+    return [];
+  }
+
 }
