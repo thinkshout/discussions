d1b40f7431573d1742388e396fa987ef59dab62f
ruscoe <danruscoe@gmail.com>
2016-09-27 20:36:09 -0700

    Add Mandrill discussions email plugin.

---
+++ modules/discussions_email/modules/discussions_email_mandrill/Plugin/DiscussionsEmailPlugin/MandrillEmailPlugin.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\discussions_email_mandrill\Plugin\MandrillEmailPlugin;
+
+use Drupal\discussions\Plugin\DiscussionsEmailPluginBase;
+
+/**
+ * Provides a content enabler for users.
+ *
+ * @DiscussionsEmailPlugin(
+ *   id = "discussions_email_mandrill",
+ *   label = @Translation("Mandrill Email Plugin for Discussions"),
+ *   description = @Translation("Allows discussions to be sent via email using Mandrill."),
+ * )
+ */
+class MandrillEmailPlugin extends DiscussionsEmailPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    $config = parent::defaultConfiguration();
+    // TODO: Any Mandrill plugin configuration.
+    return $config;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildConfigurationForm($form, $form_state);
+    // TODO: Any Mandrill plugin configuration fields.
+    return $form;
+  }
+
+}
