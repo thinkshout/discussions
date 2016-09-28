204dc32ac931227a82a02da84c43e88ac2822120
ruscoe <danruscoe@gmail.com>
2016-09-28 12:08:08 -0700

    Add config form for discussions email.

---
+++ modules/discussions_email/src/Form/DiscussionsEmailSettingsForm.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Drupal\discussions_email\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a discussions email settings form.
+ */
+class DiscussionsEmailSettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'discussions_email_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('discussions_email.settings');
+
+    // TODO: Get installed email plugins.
+    // TODO: Display notice if no email plugins installed.
+
+    $email_plugin_options = array();
+
+    $form['discussions_email_plugin_id'] = array(
+      '#type' => 'select',
+      '#title' => t('Email Plugin'),
+      '#options' => $email_plugin_options,
+      '#default_value' => $config->get('discussions_email_plugin_id'),
+    );
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    \Drupal::configFactory()->getEditable('discussions_email.settings')
+      ->set('discussions_email_plugin_id', $form_state->getValue('discussions_email_plugin_id'))
+      ->save();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['discussions_email.settings'];
+  }
+
+}
