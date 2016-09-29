0765e7ed145db614d9428b247397f0fdc10da439
ruscoe <danruscoe@gmail.com>
2016-09-28 19:41:55 -0700

    Add email plugin webhook validate / processing to webhook endpoint.

--- modules/discussions_email/src/Controller/DiscussionsEmailWebhookController.php
+++ modules/discussions_email/src/Controller/DiscussionsEmailWebhookController.php
@@ -16,8 +16,29 @@ class DiscussionsEmailWebhookController extends ControllerBase {
    * Webhook endpoint to process updates from email providers.
    */
   public function endpoint() {
-    // TODO: Load current discussions email plugin.
-    // TODO: Process update through plugin.
+    $config = \Drupal::config('discussions_email.settings');
+
+    // Get ID of the currently enabled discussions email plugin.
+    $email_plugin_id = $config->get('discussions_email_plugin_id');
+
+    // Don't attempt to process webhook update without an enabled plugin.
+    if (empty($email_plugin_id)) {
+      return Response::create(0);
+    }
+
+    /** @var \Drupal\discussions_email\DiscussionsEmailPluginManager $email_plugin_manager */
+    $email_plugin_manager = \Drupal::service('plugin.manager.discussions_email');
+
+    /** @var \Drupal\discussions_email\Plugin\DiscussionsEmailPluginInterface $plugin */
+    $plugin = $email_plugin_manager->createInstance($email_plugin_id);
+
+    // Attempt to validate webhook update source.
+    if (!$plugin->validateSource()) {
+      return Response::create(0);
+    }
+
+    // Process the webhook update.
+    $plugin->processWebhook($_REQUEST);
 
     return Response::create(1);
   }
