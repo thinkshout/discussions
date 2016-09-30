ae08c8b29bdd21113c9cd85c84466b508fab6956
ruscoe <danruscoe@gmail.com>
2016-09-29 23:07:16 -0700

    Functional discussion replies via email.

--- modules/discussions_email/modules/discussions_email_mandrill/src/Plugin/DiscussionsEmailPlugin/MandrillEmailPlugin.php
+++ modules/discussions_email/modules/discussions_email_mandrill/src/Plugin/DiscussionsEmailPlugin/MandrillEmailPlugin.php
@@ -3,7 +3,10 @@
 namespace Drupal\discussions_email_mandrill\Plugin\DiscussionsEmailPlugin;
 
 use Drupal\Component\Serialization\Json;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\discussions\Entity\Discussion;
 use Drupal\discussions_email\Plugin\DiscussionsEmailPluginBase;
+use Drupal\group\Entity\Group;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -83,6 +86,7 @@ class MandrillEmailPlugin extends DiscussionsEmailPluginBase {
     // TODO: Ignore messages sent from the discussions_email module.
 
     // Load user using the message sender's email address.
+    /** @var AccountInterface $user */
     $user = user_load_by_mail($message['from_email']);
 
     if (empty($user)) {
@@ -91,11 +95,48 @@ class MandrillEmailPlugin extends DiscussionsEmailPluginBase {
     }
 
     // Load discussion group from group email address.
+    /** @var Group $group */
     $group = $this->loadGroupFromEmail($message['email']);
 
     if (empty($group)) {
       // TODO: Log error message.
       return FALSE;
+    }
+
+    // Check user is a member of the group.
+    if (!$group->getMember($user)) {
+      return FALSE;
+    }
+
+    // TODO: Check user permission to create reply to discussion.
+    // TODO: Can group / posting access be done in DiscussionsEmailPluginBase?
+
+    $email_parts = explode('@', $message['email']);
+
+    list($email_username, $discussion_id, $parent_comment_id) = explode(self::DISCUSSION_GROUP_EMAIL_SEPARATOR, $email_parts[0]);
+
+    if (!empty($discussion_id)) {
+      // Load discussion.
+
+      /** @var \Drupal\discussions\GroupDiscussionService $group_discussion_service */
+      $group_discussion_service = \Drupal::service('discussions.group_discussion');
+
+      $discussion = $group_discussion_service->getGroupDiscussion($group->id(), $discussion_id);
+
+      if (empty($discussion)) {
+        // TODO: Log error message.
+        return FALSE;
+      }
+
+      // Add new comment to discussion.
+      $filtered_message = $this->filterEmailReply($message['html']);
+
+      // TODO: Handle replies to existing comments.
+
+      $group_discussion_service->addComment($discussion->id(), $user->id(), $filtered_message);
+    }
+    else {
+      // TODO: Create new discussion.
     }
   }
 
